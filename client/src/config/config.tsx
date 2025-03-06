a5fc5d43476316dc0b792b079f6db175da4bd582
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-03-06 10:33:48 +0000

    feat: Update config

--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -2,21 +2,8 @@ import { LeanWebConfig } from './docs' // look here for documentation of the ind
 
 const lean4webConfig : LeanWebConfig = {
   "projects": [
-    { "folder": "mathlib-demo",
-      "name": "Latest Mathlib",
-      "examples": [
-        { "file": "MathlibLatest/Bijection.lean",
-          "name": "Bijection" },
-        { "file": "MathlibLatest/Logic.lean",
-          "name": "Logic" },
-        { "file": "MathlibLatest/Ring.lean",
-          "name": "Ring" },
-        { "file": "MathlibLatest/Rational.lean",
-          "name": "Rational" }]},
-    { "folder": "stable",
-      "name": "Stable Lean" },
-    { "folder": "nightly",
-        "name": "Nightly" }
+    { "folder": "PhysLeanLatest",
+        "name": "PhysLeanLatest" }
   ],
   "serverCountry": null,
   "contactDetails": null,
